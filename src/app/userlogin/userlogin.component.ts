da3a44b248371a7b6111304c189d9858ee917c02
nivedhanr <nivedharenahan@gmail.com>
2022-11-28 17:30:33 +0530

    userlogin

--- src/app/userlogin/userlogin.component.ts
+++ src/app/userlogin/userlogin.component.ts
@@ -13,6 +13,15 @@ export class UserloginComponent {
   {
     let data:any={"username":this.username,"password":this.password}
     console.log(data)
+
+    if (this.username=="admin" && this.password=="12345") {
+
+      alert("valid credential")
+      
+    } else {
+      alert("invalid credential")
+      
+    }
   }
 
 }
