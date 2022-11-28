68691c4646f55abf78ddf09ed63a4859ea1e71b4
nivedhanr <nivedharenahan@gmail.com>
2022-11-28 17:26:13 +0530

    login condition

--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -13,5 +13,14 @@ export class AdminloginComponent {
   {
     let data:any={"username":this.username,"password":this.password}
     console.log(data)
+
+    if (this.username=="admin" && this.password=="12345") {
+
+      alert("valid credential")
+      
+    } else {
+      alert("invalid credential")
+      
+    }
   }
 }
